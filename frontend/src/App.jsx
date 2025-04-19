424a7b92ccc71cc48f9d91044d1710b268197998
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-19 14:14:05 -0300

    estrutura inicial completa do Mage Token - frontend + backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-white bg-gray-900 h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Mage Token Sender</h1>
+    </div>
+  );
+}
+
+export default App;
