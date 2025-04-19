020ce7046d1cb946e07d9fc248c22a8320ffd0b0
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-19 18:01:50 -0300

    update: sincroniza├º├úo autom├ítica com altera├º├Áes locais

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -7,13 +7,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDvn9ou5izyZO63zzOByCXCP0UR6d0hM5M",
   authDomain: "mage-token.firebaseapp.com",
   projectId: "mage-token",
-  storageBucket: "mage-token.firebasestorage.app",
+  storageBucket: "mage-token.appspot.com", // <--- aqui estava errado
   messagingSenderId: "373789026374",
   appId: "1:373789026374:web:8e48d2d1e792f3b5fe8247",
   measurementId: "G-L1FB6ZG216"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
