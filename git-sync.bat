7a09f9da6c10988f0c805f43eb5ceed5d92ca879
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-19 14:55:55 -0300

    update: sincroniza├º├úo autom├ítica com altera├º├Áes locais

---
+++ git-sync.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\bruno\OneDrive\Área de Trabalho\Projeto novo 2025\mage-token"
+git add .
+git commit -m "update: sincronização automática com alterações locais"
+git push origin main
+pause
